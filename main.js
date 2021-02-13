97c0dd31e6581ad60dfe64a5201e4edf58130925
CoderIllusionist <75251048+CoderIllusionist@users.noreply.github.com>
2021-02-13 22:11:30 +0100

    Added option to select by country and list all countries

---
+++ main.js
@@ -0,0 +1,94 @@
+
+
+async function getCountrybyName(country) {
+    try {
+        const response = await axios.get('https://restcountries.eu/rest/v2/name/' + country);
+        return response;
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+async function getAllCountries(country) {
+    try {
+        const response = await axios.get('https://restcountries.eu/rest/v2/all');
+        return response;
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+function addCountriesToArray(response) {
+    let array = [];
+    for (let i = 0; i < response.data.length; i++) {
+        array.push(response.data[i]['name'])
+    }
+    return array;
+};
+
+function returnValues(response) {
+    const countryName = response.data[0]['name'];
+    const subRegion = response.data[0]['subregion'];
+    const population = response.data[0]['population'];
+    const capital = response.data[0]['capital'];
+    const currencies = response.data[0]['currencies'];
+    const languages = response.data[0]['languages'];
+    const flag = response.data[0]['flag'];
+
+    const arrayInfo = [countryName, subRegion, population, capital, currencies, languages, flag];
+    return arrayInfo;
+};
+
+function stringBuilderSituated(x, y, z) {
+    return `${x} is situated in ${y}. It has a population of ${z} people.`
+}
+
+function stringBuilderCapital(x) {
+    return `The capital is ${x}`
+}
+
+function spokenLanguages(languages) {
+    let countries = 'They speak ';
+    for (let i = 0; i < languages.length; i++) {
+        if (i + 1 == languages.length && languages.length != 1) {
+            countries = countries + "and " + languages[i].name + "." + "\n";
+        } else if (i + 2 == languages.length) {
+            countries = countries + languages[i].name + " ";
+        } else if (languages.length == 1) {
+            countries = countries + languages[i].name + ".";
+        } else {
+            countries = countries + languages[i].name + ", ";
+        }
+    }
+    return countries
+}
+
+// getCountrybyName('belgium').then(function (result) {
+//     const values = returnValues(result);
+//     const situated = stringBuilderSituated(values[0], values[1], values[2]);
+//     const capital = stringBuilderCapital(values[3]);
+//     const languages = values[5];
+//     const speaks = spokenLanguages(languages)
+//     console.log(speaks)
+// })
+
+getAllCountries().then(function (result) {
+    const arrayCountryNames = addCountriesToArray(result)
+    var str=''; // variable to store the options
+    arrayCountryNames.forEach(element => str += '<option value="'+element+'" />');
+    var my_list=document.getElementById("country");
+    my_list.innerHTML = str;
+})
+
+function onSelectCountry() {
+    var val = document.getElementById("countries").value;
+    var opts = document.getElementById('country').childNodes;
+    for (var i = 0; i < opts.length; i++) {
+        if (opts[i].value === val) {
+            // An item was selected from the list!
+            // yourCallbackHere()
+            alert(opts[i].value);
+            break;
+        }
+    }
+}
